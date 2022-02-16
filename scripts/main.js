abc5395f1dd731e7611bfaa871c2a5d9d1b04a7a
ZmhXn <2196399299@qq.com>
2022-02-16 16:15:40 +0800

    ceshi

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@
   const CANVASHEIGHT = 150
   const CANVASID = 'canvas'
 
-  let texts = ['MY DEAR', 'LOOK UP AT THE', 'STARRY SKY', 'ARE YOU', 'LOOKING AT THE', 'SAME STAR', 'WITH ME ?', 'HAPPY', 'CHINESE', 'VALENTINE\'S', 'DAY', 'I MISS YOU']
+  let texts = ['媳妇', '对不起', '我错了', '原谅我', '我以后乖乖地听话', '不惹你生气', 'I LOVE YOU']
 
   let canvas,
     ctx,
